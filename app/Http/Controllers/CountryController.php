5bef35fd5f27f39793a58e9040035060e41c2335
ashraf-frotan <79718476+ashraf-frotan@users.noreply.github.com>
2022-11-23 22:51:30 +0430

    Country backend(route, controller, model, migrations) ready

---
+++ app/Http/Controllers/CountryController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Country;
+use Illuminate\Http\Request;
+
+class CountryController extends Controller
+{
+    // Index
+    public function index()
+    {
+        $countries=Country::all();
+        return response()->json(['data'=>$countries,'status'=>true,'message'=>'Success']);
+    }
+
+    // Store
+    public function store(Request $request)
+    {
+        $data=$request->validate([
+            'name'=>'required|min:3',
+            'code'=>'required|min:2',
+            'phonecode'=>'required|min:2',
+        ]);
+        $country=Country::create($data);
+        return response()->json(['data'=>$country,'status'=>true,'message'=>'Country inserted successfully!']);
+    }
+
+    // Updated
+    public function update(Request $request,$id)
+    {
+        $data=$request->validate([
+            'name'=>'required|min:3',
+            'code'=>'required|min:2',
+            'phonecode'=>'required|min:2',
+        ]);
+        $country=Country::find($id);
+        $country->update($data);
+        return response()->json(['data'=>$country,'status'=>true,'message'=>'Country inserted successfully!']);
+    }
+
+    // Destroy
+    public function destroy($id)
+    {
+        $country=Country::find($id)->delete();
+        return response()->json(['data'=>$country,'status'=>true,'message'=>'Country deleted successfully!']);
+    }
+}

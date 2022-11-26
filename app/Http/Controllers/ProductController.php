ce0d1a4c54949f7647c544a81602fbf2ea086ca7
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2022-11-26 15:45:32 +0430

    Product index and destroy functions done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    // Index
+    public function index()
+    {
+        $products=Product::all();
+        return response()->json($products);
+    }
+
+    // Destroy
+    public function destroy($id)
+    {
+        $products=Product::find($id)->delete();
+        return response()->json($products);
+    }
 }

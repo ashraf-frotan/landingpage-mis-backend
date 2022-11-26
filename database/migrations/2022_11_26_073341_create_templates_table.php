b60cf78145dd1b891ae27541a3daee28f63ea13c
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2022-11-26 14:53:26 +0430

    template migration completed

--- database/migrations/2022_11_26_073341_create_templates_table.php
+++ database/migrations/2022_11_26_073341_create_templates_table.php
@@ -15,6 +15,11 @@ class CreateTemplatesTable extends Migration
     {
         Schema::create('templates', function (Blueprint $table) {
             $table->id();
+            $table->string('name',64);
+            $table->string('phone',16);
+            $table->string('image',64)->nullable();
+            $table->unsignedBigInteger('page_type_id');
+            $table->foreign('page_type_id')->references('id')->on('page_types');
             $table->timestamps();
         });
     }

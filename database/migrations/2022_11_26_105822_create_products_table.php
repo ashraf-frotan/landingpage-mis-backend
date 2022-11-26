4d18f8d1823b46955b87f2583ea9730407c3666e
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2022-11-26 15:36:23 +0430

    columns added to products migration without relationships

--- database/migrations/2022_11_26_105822_create_products_table.php
+++ database/migrations/2022_11_26_105822_create_products_table.php
@@ -15,6 +15,17 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('pcode',8);
+            $table->string('title_ar',255);
+            $table->string('title_en',255);
+            $table->text('desc_ar');
+            $table->text('desc_en');
+            $table->text('message_ar');
+            $table->text('message_en');
+            $table->string('page_link',255);
+            $table->tinyInteger('page_status')->default(0)->comment('[0=>registerd, 1=>published, 2=>unpublished]');
+            $table->tinyInteger('page_language')->default(1)->comment('[0=>both, 1=>arabic, 2=>english]');
+            $table->boolean('is_collection')->default(0);
             $table->timestamps();
         });
     }

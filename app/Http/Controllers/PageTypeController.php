d913a4b05a49d6d42c868b881e6b5d6ed04f0319
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2022-11-26 12:01:54 +0430

    Page type backend completed

--- app/Http/Controllers/PageTypeController.php
+++ app/Http/Controllers/PageTypeController.php
@@ -3,8 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\PageType;
 
 class PageTypeController extends Controller
 {
-    //
+    // Index
+    public function index()
+    {
+        $page_types=PageType::all();
+        return response()->json($page_types);
+    }
+
+    // Store
+    public function store(Request $request)
+    {
+        $data=$request->validate([
+            'name'=>'required|min:3',
+            'company_id'=>'required',
+        ]);
+        $page_type=PageType::create($data);
+        return response()->json($page_type);
+    }
+
+    // Update
+    public function update(Request $request,$id)
+    {
+        $data=$request->validate([
+            'name'=>'required|min:3',
+            'company_id'=>'required',
+        ]);
+        $page_type=PageType::find($id);
+        $page_type->update($data);
+        return response()->json($page_type);
+    }
+
+    // Destroy
+    public function destroy($id)
+    {
+        $page_type=PageType::find($id)->delete();
+        return response()->json($page_type);
+    }
 }

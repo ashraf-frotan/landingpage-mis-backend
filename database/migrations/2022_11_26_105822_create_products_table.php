2e502c8092e164be1ab7136e7c68ef63c45023a0
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2022-11-26 15:50:44 +0430

    some constraints added

--- database/migrations/2022_11_26_105822_create_products_table.php
+++ database/migrations/2022_11_26_105822_create_products_table.php
@@ -16,14 +16,14 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('pcode',8);
-            $table->string('title_ar',255);
-            $table->string('title_en',255);
-            $table->text('desc_ar');
-            $table->text('desc_en');
-            $table->text('message_ar');
-            $table->text('message_en');
+            $table->string('title_ar',255)->nullable();
+            $table->string('title_en',255)->nullable();
+            $table->text('desc_ar')->nullable();
+            $table->text('desc_en')->nullable();
+            $table->text('message_ar')->nullable();
+            $table->text('message_en')->nullable();
             $table->string('page_link',255);
-            $table->tinyInteger('page_status')->default(0)->comment('[0=>registerd, 1=>published, 2=>unpublished]');
+            $table->tinyInteger('page_status')->nullable()->default(0)->comment('[0=>registerd, 1=>published, 2=>unpublished]');
             $table->tinyInteger('page_language')->default(1)->comment('[0=>both, 1=>arabic, 2=>english]');
             $table->boolean('is_collection')->default(0);
             $table->unsignedBigInteger('template_id');

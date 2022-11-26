12c71a8f9a0b18fd53dedd8d7b378b4d4a77a94b
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2022-11-26 16:26:33 +0430

    subproduct completed

--- app/Http/Controllers/SubProductController.php
+++ app/Http/Controllers/SubProductController.php
@@ -3,8 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SubProduct;
 
 class SubProductController extends Controller
 {
-    //
+    // Index
+    public function index()
+    {
+        $sub_products=SubProduct::all();
+        return response()->json($sub_products);
+    }
+
+    // Store
+    public function store(Request $request)
+    {
+        $data=$request->validate([
+            'pcode'=>'required|min:3',
+            'title'=>'required|min:3',
+            'product_id'=>'required',
+        ]);
+        $sub_product=SubProduct::create($data);
+        return response()->json($sub_product);
+    }
+
+    // Update
+    public function update(Request $request,$id)
+    {
+        $data=$request->validate([
+            'pcode'=>'required|min:3',
+            'title'=>'required|min:3',
+            'product_id'=>'required',
+        ]);
+        $sub_product=SubProduct::find($id);
+        $sub_product->update($data);
+        return response()->json($sub_product);
+    }
+
+    // Destroy
+    public function destroy($id)
+    {
+        $sub_product=SubProduct::find($id)->delete();
+        return response()->json($sub_product);
+    }
 }
--- app/Models/SubProduct.php
+++ app/Models/SubProduct.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class SubProduct extends Model
 {
     use HasFactory;
+    protected $guarded=[];
 }

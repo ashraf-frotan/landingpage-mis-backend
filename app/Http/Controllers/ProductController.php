fd2f8f8c7bfc0f3755c067794d10eabb7aa34cb7
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2022-11-26 16:03:37 +0430

    product backend completed

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,6 +14,45 @@ class ProductController extends Controller
         return response()->json($products);
     }
 
+    // Store
+    public function store(Request $request)
+    {
+        $data=$request->validate([
+            'pcode'=>'required|min:3',
+            'title_ar'=>'required_without:title_en',
+            'title_en'=>'required_without:title_ar',
+            'desc_ar'=>'required_without:desc_en',
+            'desc_en'=>'required_without:desc_ar',
+            'message_ar'=>'required_without:message_en',
+            'message_en'=>'required_without:message_ar',
+            'page_link'=>'required|min:6',
+            'page_language'=>'required'
+            
+        ]);
+        $product=Product::create($data);
+        return response()->json($product);
+    }
+
+    // Update
+    public function update(Request $request,$id)
+    {
+        $data=$request->validate([
+            'pcode'=>'required|min:3',
+            'title_ar'=>'required_without:title_en',
+            'title_en'=>'required_without:title_ar',
+            'desc_ar'=>'required_without:desc_en',
+            'desc_en'=>'required_without:desc_ar',
+            'message_ar'=>'required_without:message_en',
+            'message_en'=>'required_without:message_ar',
+            'page_link'=>'required|min:6',
+            'page_language'=>'required'
+            
+        ]);
+        $product=Product::find($id);
+        $product->update($data);
+        return response()->json($product);
+    }
+
     // Destroy
     public function destroy($id)
     {

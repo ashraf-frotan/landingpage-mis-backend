49449d9c5fa2aeef54767b5785c5fede89beb6e8
ashraf-frotan <79718476+ashraf-frotan@users.noreply.github.com>
2022-11-28 22:44:02 +0430

    Revert "some small hcnages"
    This reverts commit 425667156edfe53878983292804ec0078473a53a.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,6 @@
                     @endauth
                 </div>
             @endif
-            <img src="images/logo/logo.JPG" />
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">

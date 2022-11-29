9313ed8d0bed5e6528627136c6ca5d388333a511
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2022-11-29 15:58:08 +0430

    image attribute added

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -9,4 +9,9 @@ class Company extends Model
 {
     use HasFactory;
     protected $fillable = ['name','logo','country_id'];
+
+    public function getLogoAttribute($value)
+    {
+        return env("APP_URL") . '/assets/images/logo/'.$value;
+    } 
 }

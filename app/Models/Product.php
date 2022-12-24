d7ec28b0ce018a8fac04b22837934de9ebd30e0f
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2022-12-24 13:11:07 +0430

    template relationship added

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -23,4 +23,8 @@ class Product extends Model
     {
         return $this->hasMany(SubProduct::class);
     }
+
+    function template(){
+        return $this->belongsTo(Template::class);
+    }
 }

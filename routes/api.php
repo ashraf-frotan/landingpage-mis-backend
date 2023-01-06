bee114e3064ff6c7cd349e241804edcb82451cfb
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2023-01-06 11:32:35 +0430

    login route added

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\TemplateController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SubProductController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,11 +22,10 @@ use App\Http\Controllers\UserController;
 |
 */
 
-// Route::post('register', [RegisterController::class, 'register']);
-// Route::post('login', [RegisterController::class, 'login']);
+Route::post('login', [AuthController::class, 'login']);
      
 Route::middleware('auth:sanctum')->group( function () {
-    Route::resource('products', ProductController::class);
+    // Route::resource('products', ProductController::class);
 });
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
